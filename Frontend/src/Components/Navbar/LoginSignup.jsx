e88af6db08051e41c50c13ccb531bc0873e59103
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-06-09 06:30:01 +0530

    Library Project

--- Frontend/src/Components/Navbar/LoginSignup.jsx
+++ Frontend/src/Components/Navbar/LoginSignup.jsx
@@ -5,8 +5,10 @@ const LoginSignup = () => {
         <>
             <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    <div className="rounded-full">
+
+                        {/* hamburger icon */}
+                        <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
                     </div>
                 </div>
                 <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
