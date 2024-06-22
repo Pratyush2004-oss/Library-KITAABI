8c573e812e17009cffa2c0a884b438ebc6b42646
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-06-22 05:58:10 +0530

    Library Project

--- Frontend/src/Components/Libraries/BookSeearch.jsx
+++ Frontend/src/Components/Libraries/BookSeearch.jsx
@@ -33,15 +33,13 @@ const BookSearch = ({ book }) => {
                 </div>
                 <div className='flex'>
                     <p className='text-lg card-title font-semibold m-2'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="lightblue" className='w-7 h-7'><path d="M16 2L21 7V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918C3 2.44405 3.44495 2 3.9934 2H16ZM11 7V9H13V7H11ZM11 11V17H13V11H11Z"></path></svg>
                     </p>
-                    <p className='card-title text-lg font-bold m-2'>{book.LibraryName}</p>
+                    <p className='card-title text-lg font-bold m-2'>Library Name : {book.LibraryName}</p>
                 </div>
                 <div className='flex'>
                     <p className='text-lg card-title font-semibold m-2'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="lightblue" className='w-7 h-7'><path d="M16 2L21 7V21.0082C21 21.556 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918C3 2.44405 3.44495 2 3.9934 2H16ZM11 7V9H13V7H11ZM11 11V17H13V11H11Z"></path></svg>
                     </p>
-                    <p className='card-title text-lg font-bold m-2'>{book.Address}</p>
+                    <p className='card-title text-lg font-bold m-2'>Library Address : {book.Address}</p>
                 </div>
             </div>
         </div>
