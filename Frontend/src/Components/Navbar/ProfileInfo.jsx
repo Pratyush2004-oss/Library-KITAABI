c839a03e0212e0fc805427743005f3485358fa17
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-06-29 22:26:16 +0530

    Display Changes in Library Project

--- Frontend/src/Components/Navbar/ProfileInfo.jsx
+++ Frontend/src/Components/Navbar/ProfileInfo.jsx
@@ -8,15 +8,15 @@ const ProfileInfo = () => {
   const { authUser } = useAuthContext();
 
   return (
-    <div className="flex-none mx-5 px-5 gap-5">
-      <div className=" flex-col items-center dropdown-hover dropdown dropdown-end flex-1">
-        <div tabIndex={0} role="button" className="btn mx-3 my-1 btn-ghost btn-circle avatar">
-          <div className="w-20 items-center rounded-full ring ring-white ring-offset-base-100 ring-offset-2">
+    <div className="flex-none px-5 gap-5">
+      <div className="dropdown-hover px-5 dropdown dropdown-end flex-1">
+        <div tabIndex={0} role="button" className="btn flex m-auto my-1 btn-ghost btn-circle avatar">
+          <div className="w-20 items-center mx-auto rounded-full ring ring-white ring-offset-base-100 ring-offset-2">
             <img alt="Tailwind CSS Navbar component" src={authUser.ProfilePic} />
           </div>
         </div>
         <div>
-          <h1 className='text-lg text-center font-bold'>{(authUser.LoginType === "Reader") ? authUser.fullname : authUser.LibraryName}</h1>
+          <span className='text-lg text-center font-bold'>{(authUser.LoginType === "Reader") ? authUser.fullname : authUser.LibraryName}</span>
         </div>
         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
           <li><Link to={'/Account'}>Account Section</Link></li>
